3af41d55e7c7daf6970a0e2f8a41f2fdd1bd6127
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2024-08-02 14:34:04 +0100

    Set up database connection

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,10 @@
-// add your database connection here
+const path = require('path')
+const { DataTypes, Model, Sequelize } = require('sequelize')
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  logging: false,
+  storage: path.join(__dirname, '..', 'db.sqlite')
+})
+
+module.exports = { DataTypes, Model, db }
